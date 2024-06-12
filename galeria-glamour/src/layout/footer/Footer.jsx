b7e279d0ea3d32f428635f7285097b27560cc4c7
JULIAN-PC <julian.francama@gmail.com>
2024-06-12 01:02:20 -0300

    layout completo faltan estilos, a definir

--- galeria-glamour/src/layout/footer/Footer.jsx
+++ galeria-glamour/src/layout/footer/Footer.jsx
@@ -1,7 +1,27 @@
 import './Footer.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faPaperPlane } from '@fortawesome/free-regular-svg-icons';
+import { faInstagram, faSquareFacebook, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 
 export default function Footer() {
     return (
-        <footer>FOOTER</footer>
+        <footer className="main-footer">
+            <section className="footer-section footer-brand">
+                <img src="" className="footer-logo" alt="Logo"/>
+            </section>
+            <section className="footer-section footer-social">
+                <a className="footer-social-link" href="h" target="_blank"><FontAwesomeIcon icon={faInstagram} size='2xl' /></a>
+                <a className="footer-social-link" href="" target="_blank"><FontAwesomeIcon icon={faSquareFacebook} size="2xl" /></a>
+                <a className="footer-social-link" href="" target="_blank"><FontAwesomeIcon icon={faWhatsapp} size="2xl" /></a>
+                <a className="footer-social-link" href="" target="_blank"><FontAwesomeIcon icon={faEnvelope} size="2xl" /></a>
+            </section>
+            <section className="footer-section footer-contact">
+                <label className="newsletter">¡Sus!</label>
+                <div className="suscribe">
+                    <input className="email" type="email" />
+                    <button className="btn-newsletter" type="submit"> <FontAwesomeIcon icon={faPaperPlane} /> </button>
+                </div>
+            </section>
+        </footer>
     )
 }
--- galeria-glamour/src/layout/modal/Modal.jsx
+++ galeria-glamour/src/layout/modal/Modal.jsx
@@ -1,1 +1,20 @@
 import './Modal.css'
+
+export default function Modal({title, handleClose, isOpen, children}){
+
+
+    if(!isOpen) return; /* Si no esta abierto devuelvo null por lo que no pinta el return de abajo*/
+
+    return(
+        <div className="modal-overlay" onClick={handleClose}> {/* Tambien aqui puedo poner para que cierre clickeando fuera del modal */}
+            <div className="modal-content" onClick={(e) => e.stopPropagation()}> {/* Evito propagación para que no aplique el handleStop de overlay */}
+                <div className="modal-header">
+                    {title}
+                </div>
+                <div className="modal-body">
+                    {children}
+                </div>
+            </div>
+        </div>
+    )
+}
